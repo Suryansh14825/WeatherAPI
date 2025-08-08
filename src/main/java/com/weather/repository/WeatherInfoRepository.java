dc66c269b48bf062a92143f0f993b9bda8b27e65
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:24:53 +0530

    Update WeatherInfoRepository.java

--- src/main/java/com/weather/repository/WeatherInfoRepository.java
+++ src/main/java/com/weather/repository/WeatherInfoRepository.java
@@ -1,4 +1,15 @@
 package com.weather.repository;
 
-public interface WeatherInfoRepository {
+import com.weather.model.PincodeLocation;
+import com.weather.model.WeatherInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface WeatherInfoRepository extends JpaRepository<WeatherInfo, Long> {
+    Optional<WeatherInfo> findByPincodeLocationAndDate(PincodeLocation pincodeLocation, LocalDate date);
 }
+
