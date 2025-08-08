458c0c48e51b60a7df17e18599415de211dd709e
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:23:32 +0530

    Update PincodeLocation.java

--- src/main/java/com/weather/model/PincodeLocation.java
+++ src/main/java/com/weather/model/PincodeLocation.java
@@ -1,4 +1,52 @@
 package com.weather.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class PincodeLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String pincode;
+    private double latitude;
+    private double longitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+// Getters and Setters
 }
+
