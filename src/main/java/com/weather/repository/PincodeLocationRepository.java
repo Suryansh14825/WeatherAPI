3349e357042cd3c6de0c7c735e242c42efb9151b
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:24:30 +0530

    Update PincodeLocationRepository.java

--- src/main/java/com/weather/repository/PincodeLocationRepository.java
+++ src/main/java/com/weather/repository/PincodeLocationRepository.java
@@ -1,4 +1,13 @@
 package com.weather.repository;
 
-public class PincodeLocationRepository {
+import com.weather.model.PincodeLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PincodeLocationRepository extends JpaRepository<PincodeLocation, Long> {
+    Optional<PincodeLocation> findByPincode(String pincode);
 }
+
