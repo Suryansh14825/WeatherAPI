ad10167d769dcb673f38a89711d609a606f4e69c
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:23:03 +0530

    Update WeatherApiClientImp.java

--- src/main/java/com/weather/external/WeatherApiClientImp.java
+++ src/main/java/com/weather/external/WeatherApiClientImp.java
@@ -1,4 +1,26 @@
 package com.weather.external;
 
-public class WeatherApiClientImp {
+import com.weather.external.WeatherApiClient;
+import com.weather.dto.WeatherApiResponseDTO;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WeatherApiClientImp implements WeatherApiClient {
+
+    @Value("${openweather.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Override
+    public WeatherApiResponseDTO getWeather(double lat, double lon) {
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat +
+                "&lon=" + lon + "&appid=" + apiKey + "&units=metric";
+
+        return restTemplate.getForObject(url, WeatherApiResponseDTO.class);
+    }
 }
+
