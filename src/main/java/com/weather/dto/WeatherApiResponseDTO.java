02903ee86ce973bca89008d5079b5919fb280c29
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:18:22 +0530

    Updated

--- src/main/java/com/weather/dto/WeatherApiResponseDTO.java
+++ src/main/java/com/weather/dto/WeatherApiResponseDTO.java
@@ -1,9 +1,11 @@
 package com.weather.dto;
 
-public class LocationResponse {
+public class WeatherResponseDTO {
     private String pincode;
-    private double latitude;
-    private double longitude;
+    private String city;
+    private String date;
+    private double temperature;
+    private String description;
 
     public String getPincode() {
         return pincode;
@@ -13,19 +15,36 @@ public class LocationResponse {
         this.pincode = pincode;
     }
 
-    public double getLatitude() {
-        return latitude;
+    public String getCity() {
+        return city;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
+    public void setCity(String city) {
+        this.city = city;
     }
 
-    public double getLongitude() {
-        return longitude;
+    public String getDate() {
+        return date;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
+    public void setDate(String date) {
+        this.date = date;
     }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+// Getters and Setters
 }
