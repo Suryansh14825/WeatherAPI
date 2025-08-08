87dbd47ed33ef5bb28e4854797c7692a1f5aac56
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:21:49 +0530

    Update WeatherResponseDTO.java

--- src/main/java/com/weather/dto/WeatherResponseDTO.java
+++ src/main/java/com/weather/dto/WeatherResponseDTO.java
@@ -1,4 +1,51 @@
 package com.weather.dto;
 
 public class WeatherResponseDTO {
+    private String pincode;
+    private String city;
+    private String date;
+    private double temperature;
+    private String description;
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+// Getters and Setters
 }
+
