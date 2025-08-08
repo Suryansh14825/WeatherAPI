62f356fad8b7427ff76791f57fc30eba0ee0e0b9
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:18:56 +0530

    Updated

--- src/main/java/com/weather/dto/WeatherRequestDTO.java
+++ src/main/java/com/weather/dto/WeatherRequestDTO.java
@@ -1,4 +1,27 @@
 package com.weather.dto;
 
 public class WeatherRequestDTO {
+    private String pincode;
+    private String forDate;
+
+    // Getters and Setters
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public String getForDate() {
+        return forDate;
+    }
+
+    public void setForDate(String forDate) {
+        this.forDate = forDate;
+    }
+
+
 }
+
