7be8169ae720a4eba4d2f269a0c3e8dbd655fa21
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:21:08 +0530

    Update WeatherController.java

--- src/main/java/com/weather/controller/WeatherController.java
+++ src/main/java/com/weather/controller/WeatherController.java
@@ -1,4 +1,24 @@
 package com.weather.controller;
 
+import com.weather.dto.WeatherRequestDTO;
+import com.weather.dto.WeatherResponseDTO;
+import com.weather.model.PincodeLocation;
+import com.weather.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/weather")
 public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @PostMapping
+    public ResponseEntity<WeatherResponseDTO> getWeather(@RequestBody WeatherRequestDTO request) {
+        WeatherResponseDTO response = weatherService.getWeather(request.getPincode(), request.getForDate());
+        return ResponseEntity.ok(response);
+    }
 }
+
